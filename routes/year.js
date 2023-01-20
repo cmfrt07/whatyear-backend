333aa1aca78dd618a22c3f7463a23b6cbc11888c
cam.frt07@gmail.com <99878415+6oldenBerry@users.noreply.github.com>
2023-01-20 10:28:35 +0100

    my first commit

---
+++ routes/year.js
@@ -0,0 +1,11 @@
+var express = require('express');
+const { route } = require('.');
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  const newYear = new Date().getFullYear
+  res.json({year: newYear });
+});
+
+
+module.exports = router;
