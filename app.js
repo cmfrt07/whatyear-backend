cb67feab3f2d0bf2f59a95abeb3c2fd99f809f20
cam.frt07@gmail.com <99878415+6oldenBerry@users.noreply.github.com>
2023-01-20 11:20:01 +0100

    commit final

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/year', usersRouter);
+app.use('/year', yearRouter);
 
 module.exports = app;
--- routes/year.js
+++ routes/year.js
@@ -3,7 +3,7 @@ const { route } = require('.');
 var router = express.Router();
 
 router.get("/", (req, res) => {
-  const newYear = new Date().getFullYear
+  const newYear = new Date().getFullYear()
   res.json({year: newYear });
 });
 
